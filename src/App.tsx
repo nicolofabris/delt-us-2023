d4e280086c8ec292b32dee4436f5e7badb2ece9b
nicolofabris <nicolo.fabriss@gmail.com>
2023-05-03 17:25:35 +0200

    third commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./scenes/navbar/index";
 import { SelectedPage } from "./shared/type";
+import Home from './scenes/home/index';
 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) {
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      }
+      if ( window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Home setSelectedPage={setSelectedPage}/>
     </div>
-  );
+  )
 }
 
 export default App;
